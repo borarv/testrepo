e19d038bd4dd810760b458337ec565e0aedf7603
venkbora <venkbora@in.ibm.com>
2019-09-16 07:06:07 +0000

    First Commit

---
+++ calc.py
@@ -0,0 +1,16 @@
+def add(x, y):
+    """Add Function"""
+       return x + y
+
+def subtraction(x, y):
+    """substract Function"""
+       return x - y
+   
+def multiply(x, y):
+    """multiply Function"""
+       return x * y
+
+def divide(x, y):
+    """divide Function"""
+       return x / y
+
